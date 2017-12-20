828258b93b5547fe7e3031dacd16abf49fbd6757
Jason Tucker <jtucker@gmail.com>
2017-12-20 13:51:32 -0500

    Start on day 2 challenge

---
+++ adventofcode2017/day2.fs
@@ -0,0 +1,8 @@
+﻿module AdventOfCode.Day2
+open Xunit
+
+[<Theory>]
+[<InlineData("day2_theory.txt", 18)>]
+let ``should solve checksum`` (input, output) =
+    let actual = input |> int
+    Assert.Equal<int>(output, actual)

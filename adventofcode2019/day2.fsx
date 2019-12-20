792091937eb4bc8e6bf3432e3d8b0bb8ce9fc00e
Jason Tucker <jtucker@gmail.com>
2019-12-20 09:33:27 -0500

    completed day2 part 1

--- adventofcode2019/day2.fsx
+++ adventofcode2019/day2.fsx
@@ -1,12 +1,31 @@
 open System.IO
 
-let inputData = 
-    File.ReadAllText("adventofcode2019\\inputdata\\day2.txt").Split(',')
+let mutable inputData = 
+    File.ReadAllText("inputdata\\day2.txt").Split(',')
     |> Array.map int
 
-type OpCodes = 
-    | Addition = 1
-    | Multiplication = 2
-    | EndProgram = 99
+type ProcessedData = 
+    {
+        data : int array        
+    }
 
-Array.splitInto
+let initial = 
+    {
+        data = inputData
+    }
+
+let updateState state operation x y p =
+    state.data.[p] <- operation state.data.[x] state.data.[y]
+    state
+
+let folder state opcode =
+    match opcode |> Array.toList with
+    | x :: xs when x = 1 && xs.Length = 3 -> updateState state (+) xs.[0] xs.[1] xs.[2]
+    | x :: xs when x = 2 && xs.Length = 3 -> updateState state (*) xs.[0] xs.[1] xs.[2]
+    | _ -> state
+
+let processed = inputData
+                |> Array.chunkBySize 4
+                |> Array.fold folder initial
+
+printfn "value at position 0: %i" processed.data.[0]

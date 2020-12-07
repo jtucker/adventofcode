7847d51a78eabc602d37029ed5e163a1f02b5ecd
Jason Tucker <jtucker@gmail.com>
2020-12-06 23:42:34 -0500

    completed Day 3 part 2

--- 2020/Day3.fs
+++ 2020/Day3.fs
@@ -1,23 +1,30 @@
 module codingforbeer.AdventOfCode.Day3
 
 type Position = {
-    X: int
-    Y: int
+    First: int
+    Second: int
 }
 
-let assetContent = fileContents @"2020\assets\day3.txt" |> List.ofSeq
-let startPosition = { X = 0; Y = 0 }
+let arrContent = fileContents @"2020\assets\day3.txt" |> Array.ofSeq |> array2D
+let slopes = [(1, 1); (3, 1); (5, 1); (7, 1); (1, 2)]
+let startState = ({ First = 0; Second = 0 }, 0)
 
-let movePosition position =
-    { position with X = position.X + 3; Y = position.Y + 1 }
+let movePosition slope position maxLength =
+    let (right, down) = slope
+    match position with
+    | p when p.First + down > maxLength -> None
+    | _ -> Some ({ position with First = position.First + down; Second = position.Second + right })
 
 let isTree =
     function | '#' -> 1
              | _ -> 0
 
-let folder (p, t) (l: string) =
-    System.Console.WriteLine (sprintf "%A" p)
-    (movePosition p, t + (isTree l.[p.X % 31]))
+let rec arrFold slope state (array: char [,]) =
+    let (position: Position), (trees:int) = state
+    match movePosition slope position (Array2D.length1 array) with
+    | None -> state
+    | Some (p) -> arrFold slope (p, trees + (isTree array.[position.First, position.Second % 31])) array
 
-let _, treeCount = assetContent |> List.fold (folder) (startPosition, 0)
-System.Console.WriteLine ( sprintf "You would hit %i trees" treeCount )
+let calcTrees =
+    let treeMapper = (fun s -> arrFold s startState arrContent) >> (fun (p, trees) -> int64 trees)
+    slopes |> List.map (treeMapper) |> List.reduce ((*))

62f8c3a845d1aea99819e0654f12345c14045bbd
Jason Tucker <jtucker@gmail.com>
2020-12-19 10:25:30 -0500

    Day 6 part 2 complete

--- 2020/Day6.fs
+++ 2020/Day6.fs
@@ -2,13 +2,31 @@ module codingforbeer.AdventOfCode.Day6
 open System
 
 let responses = fileText @"2020\assets\day6.txt"
+let blankLineSeperator = Environment.NewLine + Environment.NewLine
 
 let yesAnswerCounts (group: string) =
     let isLetter = function
         | '\013' | '\010' -> false
         | _ -> true
+    let getYesAnswerCountForGroup g =
+        g |> Seq.toList |> List.distinct |> List.filter isLetter |> List.length
 
-    group.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
-    |> Array.map(fun s -> s |> Seq.toList |> List.distinct |> List.filter isLetter |> List.length)
+    group.Split(blankLineSeperator, StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+    |> Array.map getYesAnswerCountForGroup
 
 printfn "Sum total of groups yes answers: %i" (yesAnswerCounts responses |> Array.sum)
+
+let getEveryoneYesAnswers (group: string) =
+    let getAllSameAnswerForGroup (g: string) =
+        g.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries) 
+        |> Array.toList
+        |> List.map Set.ofSeq
+        |> List.reduce (Set.intersect)
+        |> Set.count
+        
+
+    group.Split(blankLineSeperator, StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries) 
+    |> Array.toList
+    |> List.map (getAllSameAnswerForGroup)
+
+printfn "Sum total of groups same answers: %i" (getEveryoneYesAnswers responses |> List.sum)

58ab4bdcf67d950bc0f30d552f2838b57568df3b
Jason Tucker <jtucker@gmail.com>
2020-12-27 23:24:28 -0500

    finish day 8 part 2

--- 2020/Day8.fs
+++ 2020/Day8.fs
@@ -1,6 +1,5 @@
 module codingforbeer.AdventOfCode.Day8
 
-open System
 open FParsec
 
 let demo = "nop +0
@@ -13,30 +12,65 @@ acc +1
 jmp -4
 acc +6"
 
+type Action = 
+    | Accumulator
+    | Jump
+    | NoOperation
+
+type Execution =
+    | Duplicate of int
+    | InfiniteLoop
+    | Exited of int
+
 type State = {
     Acc: int
+    PreviousIndex: int
     CurrIndex: int
     Visited: int list
 }
-let state = { Acc = 0; CurrIndex = 0; Visited = []; }
 
-let check state cmd = 
-    let action, v = cmd
-    match action with
-    | "acc" -> { state with Acc = (state.Acc + (int32 v)); Visited = (state.Visited @ [state.CurrIndex]) }
-    | "jmp" -> { state with CurrIndex = (int32 v); Visited = (state.Visited @ [state.CurrIndex])}
-    | _ -> state
+let state = { Acc = 0; PreviousIndex = 0; CurrIndex = 0; Visited = List.empty; }
 
 let parseCmd cmd =
     let actionParser = choice [
-        pstring "acc"
-        pstring "jmp"
-        pstring "nop"
+        stringReturn "acc" Accumulator
+        stringReturn "jmp" Jump
+        stringReturn "nop" NoOperation
     ]
     let parser = sepBy (actionParser .>> spaces1 .>>. pint32) newline
     match run parser cmd with
-    | Success (v, _, _) -> v |> List.mapi ( fun i (action, value) -> (i, action, value))
+    | Success (v, _, _) -> v |> List.mapi (fun i (action, value) -> (i, action, value))
     | Failure _ -> failwith "parsing error"
 
-demo.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries) 
-|> Array.map parseCmd
+let rec getAccumulator (state: State) (actions: (int * Action * int) list) =
+    if state.CurrIndex = actions.Length then Exited state.Acc 
+    else
+        let nextIndex = state.CurrIndex + 1
+        let idx, action, value = actions.[state.CurrIndex]
+        
+        match action with
+        | _ when List.contains state.CurrIndex state.Visited -> Duplicate state.Acc
+        | Jump when state.CurrIndex = (state.CurrIndex + value) -> InfiniteLoop
+        | NoOperation -> getAccumulator { state with Visited = state.Visited @ [idx]; CurrIndex = nextIndex; PreviousIndex = state.CurrIndex } actions 
+        | Accumulator -> getAccumulator { state with Acc = state.Acc + value; CurrIndex = nextIndex; Visited = state.Visited @ [idx]; PreviousIndex = state.CurrIndex } actions 
+        | Jump -> getAccumulator { state with CurrIndex = state.CurrIndex + value; Visited = state.Visited @ [idx]; PreviousIndex = state.CurrIndex } actions
+
+let getExecution (actions: (int * Action * int) list) =
+    let listOfJmpNop (actions: (int * Action * int) list) =
+        let filter item = 
+            let _, action, _ = item
+            match action with
+            | NoOperation | Jump -> true
+            | _ -> false    
+        actions |> List.filter filter
+
+    let updateList (actions: (int * Action * int) list) (idx, action, value) =
+        let flipAction = function
+            | NoOperation -> Jump
+            | Jump -> NoOperation
+            | Accumulator -> Accumulator
+        actions.[0..idx - 1] @ [(idx, flipAction action, value)] @ actions.[idx + 1..]
+
+    listOfJmpNop actions
+    |> List.map ((updateList actions) >> (getAccumulator state)) 
+    |> List.filter(function | Exited _ -> true | _ -> false);;
